61c32109f3ffc967e70b4f94228ea7cc2a3f03f1
Petr Volf <Petr.volf216@gmail.com>
2021-08-28 19:41:36 +0200

    day 15 progress

--- src/days/day_15.rs
+++ src/days/day_15.rs
@@ -1,10 +1,29 @@
 use std::{
-    collections::{HashSet, VecDeque},
+    collections::{HashMap, VecDeque},
     convert::TryFrom,
     ops::Index,
 };
 
 const INPUT: &str = include_str!("../inputs/day_15_input");
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Position {
+    fn manhattan_distiance_to(&self, other: &Position) -> usize {
+        let x = (self.x as i32 - other.x as i32).abs();
+        let y = (self.y as i32 - other.y as i32).abs();
+
+        (x + y) as usize
+    }
+
+    fn from_tuple((x, y): (usize, usize)) -> Self {
+        Self { x, y }
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Tile {
@@ -25,52 +44,72 @@ impl TryFrom<u8> for Tile {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 enum EntityType {
     Goblin,
     Elf,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Entity {
     hp: u32,
     attack: u32,
     side: EntityType,
-    x: usize,
-    y: usize,
+    pos: Position,
 }
 
 impl Entity {
     fn new(x: usize, y: usize, side: EntityType) -> Self {
         Self {
-            x,
-            y,
+            pos: Position { x, y },
             side,
             attack: 3,
             hp: 200,
         }
     }
 
-    fn list_targets<'a>(&self, entities: &'a [Entity]) -> impl Iterator<Item = &'a Entity> {
+    fn list_targets<'a>(&self, entities: &'a [Entity]) -> Vec<&'a Entity> {
         let filter = match self.side {
             EntityType::Elf => |e: &&Entity| e.side == EntityType::Goblin,
             EntityType::Goblin => |e: &&Entity| e.side == EntityType::Elf,
         };
 
-        entities.iter().filter(filter)
+        entities.iter().filter(filter).collect::<Vec<_>>()
     }
 
-    fn accesible_targets(&self, entities: &[Entity], map: &Map) -> Vec<(usize, usize)> {
-        let rtree = build_reach_tree((self.x, self.y), &entities, map);
+    fn locate_target(&self, reach_tree: &ReachTree, targets: &[&Entity], map: &Map) -> Option<Entity> {
+        let mut positions = targets
+            .iter()
+            .flat_map(|e| find_pos_in_range(e.pos.x, e.pos.y, map))
+            .filter(|pos| reach_tree.tree.contains_key(&Position::from_tuple(*pos)))
+            .collect::<Vec<_>>();
+        positions.sort();
+        positions.dedup();
 
-        self.list_targets(&entities)
-            .flat_map(|e| find_pos_in_range(e.x, e.y, map))
-            .filter(|to| rtree.contains(to))
-            .collect::<Vec<_>>()
-    }
+        // find closest
+        let min = positions.values().min().unwrap();
 
-    fn locate_target(&self, entities: &[Entity], map: &Map) -> Option<Entity> {
+        let mut closest = Vec::new();
+        for pos in &positions {
+            let pos_cost = reach_tree.tree.get(&Position::from_tuple(*pos));
+            if let Some(cost) = pos_cost {
+                if cost == min {
+                    closest.push(pos);
+                }
+            }
+        }
+
+        dbg!(closest);
+
         todo!()
+    }
+
+    fn move_to_target(&self, reach: &ReachTree, target: &Entity) {
+        todo!();
+    }
+
+    fn attack_entity(&self, other: &mut Entity) {
+        other.hp -= self.attack;
     }
 }
 
@@ -148,85 +187,89 @@ fn find_pos_in_range(x: usize, y: usize, map: &Map) -> Vec<(usize, usize)> {
     output
 }
 
-/*fn reachable((to_x, to_y): &(usize, usize), (from_x, from_y): (usize, usize), entities: &[Entity], map: &Map) -> bool {
-    let walkable = |x: usize, y: usize| map[(x, y)] == Tile::Floor && !entities.iter().any(|a| a.x == x && a.y == y);
-
-    // TODO write pathfinding
-    unimplemented!();
-}*/
-
-fn build_reach_tree((from_x, from_y): (usize, usize), entities: &[Entity], map: &Map) -> HashSet<(usize, usize)> {
-    let mut frontier = VecDeque::new();
-    frontier.push_back((from_x, from_y));
-    let mut visited = HashSet::new();
-
-    while !frontier.is_empty() {
-        // unwrap can be done, because this loop won't happen if frontier is empty
-        let pos = frontier.pop_front().unwrap();
-        visited.insert(pos);
-        let neighbours = find_pos_in_range(pos.0, pos.1, map);
-        for n in neighbours {
-            if !frontier.contains(&n) && !visited.contains(&n) && !entities.iter().any(|e| e.x == n.0 && e.y == n.1) {
-                frontier.push_back(n);
-            }
-        }
-    }
-
-    visited
+struct ReachTree {
+    tree: HashMap<Position, i32>,
 }
 
-#[allow(dead_code)]
-fn render_reach_tree(tree: &HashSet<(usize, usize)>) {
-    let min_x = tree.iter().min_by(|a, b| a.0.cmp(&b.0)).unwrap().0;
-    let min_y = tree.iter().min_by(|a, b| a.1.cmp(&b.1)).unwrap().1;
+impl ReachTree {
+    fn build_from_pos(pos: &Position, entities: &[Entity], map: &Map) -> Self {
+        let mut frontier = VecDeque::new();
+        frontier.push_back((pos.x, pos.y, -1));
+        let mut tree = HashMap::new();
 
-    let max_x = tree.iter().max_by(|a, b| a.0.cmp(&b.0)).unwrap().0;
-    let max_y = tree.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap().1;
-
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            if tree.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(" ");
+        while let Some(front) = frontier.pop_front() {
+            tree.insert(Position { x: front.0, y: front.1 }, front.2 + 1);
+            let neighbours = find_pos_in_range(front.0, front.1, map);
+            for n in neighbours {
+                if !frontier.iter().any(|(x, y, _cost)| *x == n.0 && *y == n.1)
+                    && !tree.contains_key(&Position { x: n.0, y: n.1 })
+                    && !entities.iter().any(|e| e.pos.x == n.0 && e.pos.y == n.1)
+                {
+                    frontier.push_back((n.0, n.1, front.2 + 1));
+                }
             }
         }
-        println!();
-    }
-}
 
-fn man_dist(from: (usize, usize), to: (usize, usize)) -> usize {
-    ((from.0 as i32 - to.0 as i32).abs() + (from.1 as i32 - to.1 as i32).abs()) as usize
+        Self { tree }
+    }
+
+    #[allow(dead_code)]
+    /// Renders reach tree to terminal
+    fn render(&self) {
+        let min_x = self.tree.iter().min_by(|(a, _), (b, _)| a.x.cmp(&b.x)).unwrap().0.x;
+        let min_y = self.tree.iter().min_by(|(a, _), (b, _)| a.y.cmp(&b.y)).unwrap().0.y;
+
+        let max_x = self.tree.iter().max_by(|(a, _), (b, _)| a.x.cmp(&b.x)).unwrap().0.x;
+        let max_y = self.tree.iter().max_by(|(a, _), (b, _)| a.y.cmp(&b.y)).unwrap().0.y;
+
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                let tile = self.tree.get(&Position { x, y });
+                match tile {
+                    Some(cost) => print!("{:3}", cost),
+                    None => print!("   "),
+                }
+            }
+            println!();
+        }
+    }
 }
 
 pub fn solve() {
     let (map, mut entities) = Map::from_str_with_entities(INPUT);
+    let mut round = 0;
     'turn: loop {
         for entity in &entities {
-            let mut targets = entity.accesible_targets(&entities, &map);
+            let targets = entity.list_targets(&entities);
             if targets.is_empty() {
-                continue;
+                break 'turn;
             }
-            //dedup only removes consecutive duplicates
-            targets.sort_by(|a, b| a.1.cmp(&b.1).then(a.0.cmp(&b.0)));
-            targets.dedup();
 
-            // sort by distance
-            // TODO STUPID FIX - distance does not have map info
-            let mut targets = targets
-                .iter()
-                .map(|pos| (man_dist(*pos, (entity.x, entity.y)), *pos))
-                .collect::<Vec<_>>();
-            targets.sort_by(|a, b| a.0.cmp(&b.0));
-            let targets = targets.iter().map(|(_, pos)| *pos).collect::<Vec<_>>();
+            let reach_tree = ReachTree::build_from_pos(&entity.pos, &entities, &map);
 
-            let target = targets[0];
-            dbg!(target);
+            // now select target from list
+            let mut target = entity.locate_target(&reach_tree, &targets, &map);
+
+            if let Some(ref mut target) = target {
+                // move if needed
+                let close = false;
+                entity.move_to_target(&reach_tree, &target);
+
+                if close {
+                    entity.attack_entity(target);
+                }
+            }
         }
-        entities.sort_by(|a, b| a.y.cmp(&b.y).then(a.x.cmp(&b.x)));
-        break 'turn;
+
+        // remove dead entities
+        entities.retain(|e| e.hp > 0);
+
+        entities.sort_by(|a, b| a.pos.y.cmp(&b.pos.y).then(a.pos.x.cmp(&b.pos.x)));
+        round += 1;
     }
-    //dbg!(map, entities);
+    let total_hp = entities.iter().fold(0, |total, entity| total + entity.hp);
+
+    println!("Outcome: {} * {} = {}", round, total_hp, round * total_hp);
 }
 
 pub fn solve_extra() {}
